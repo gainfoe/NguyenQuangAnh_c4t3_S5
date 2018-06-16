8a2a33de1c29dd7c1092130a9ba8663937b3ae0d
gainfoe <39850946+gainfoe@users.noreply.github.com>
2018-06-17 05:42:12 +0700

    Delete Lesson_5_1.py

--- Lesson_5_1.py
+++
@@ -1,9 +0,0 @@
-def tim_max(arr_):
-    maximum = arr_[0]
-    for i in range(len(arr_)):
-        if arr_[i] > maximum:
-            maximum = arr_[i]
-    return maximum
-
-def chu_vi(a, b, c):
-    return a + b + c
